5e0b34e21fe309ea0e4dee2ef0376ed574ba3e40
MilenaKostic <101191564+MilenaKostic@users.noreply.github.com>
2023-11-22 01:42:11 +0100

    StopsView with map

--- WebApplication-BusTimeline/Pages/Stops/StopsView.cshtml.cs
+++ WebApplication-BusTimeline/Pages/Stops/StopsView.cshtml.cs
@@ -1,12 +1,67 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Identity.Client;
+using Newtonsoft.Json;
+using Shared.DTO;
 
 namespace WebApplication_BusTimeline.Pages.Stops
 {
     public class StopsViewModel : PageModel
     {
-        public void OnGet()
+        public Int32? StanicaId { get; set; }
+        public List<StopGetBasicDTO> LstStops { get; set; }
+        public StopDTO SelectedStop { get; set; }
+
+        public async Task OnGetAsync(string stanicaId)
         {
+            using(var httpClient = new HttpClient())
+            {
+                try
+                {
+                    using(HttpResponseMessage response = await httpClient.GetAsync("https://localhost:7151/Stop"))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        var _stops = (JsonConvert.DeserializeObject<List<StopGetBasicDTO>>(apiResponse)).ToList();
+
+                        LstStops = _stops.ToList();
+
+                    }
+                }
+                catch(Exception e)
+                {
+                    //Osmisli 
+                }
+            }
+
+            StanicaId = null;
+            int _stanicaId = -1;
+
+            if(Int32.TryParse(stanicaId, out _stanicaId))
+            {
+                StanicaId = _stanicaId;
+            }
+
+            if(StanicaId != null)
+            {
+                using(var httpClient = new HttpClient())
+                {
+                    try
+                    {
+                        using (HttpResponseMessage response = await httpClient.GetAsync($"https://localhost:7151/Stop/ById:{StanicaId}"))
+                        {
+                            string apiResponse = await response.Content.ReadAsStringAsync();
+                            SelectedStop = JsonConvert.DeserializeObject<StopDTO>(apiResponse);
+                        }
+                    }
+                    catch(Exception e)
+                    {
+                        //Osmisli
+                    }
+                }
+            }
+
         }
+
+        
     }
 }
