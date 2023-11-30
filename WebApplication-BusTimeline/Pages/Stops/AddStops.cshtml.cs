6da27efc8c2ae164ba4217c7dde90f86556d2831
MilenaKostic <101191564+MilenaKostic@users.noreply.github.com>
2023-11-30 22:38:26 +0100

    Modified AddStop View, added input for StopName

--- WebApplication-BusTimeline/Pages/Stops/AddStops.cshtml.cs
+++ WebApplication-BusTimeline/Pages/Stops/AddStops.cshtml.cs
@@ -1,40 +1,89 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using Shared.DTO;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 using WebAPI.Data;
 using WebAPI.Models;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace WebApplication_BusTimeline.Pages.Stops
 {
     public class AddStopsModel : PageModel
     {
-		private readonly DataContext context;
-
-        public double lat = -300;
-        public double Long = -300;
-
+			
 		public string? ErrorMessage {  get; set; }
 
-        public AddStopsModel(DataContext context)
-        {
-			this.context = context;
-		}
+		public double lat {  get; set; }
+		public double lon { get; set; }
 
-        public async Task OnGetAsync(double? lat)
+		List<StopGetBasicDTO> stops = new List<StopGetBasicDTO>();
+      
+        public async Task OnGetAsync()
 		{
-            if(lat != -300 )
-            {
-				Console.WriteLine($"Vrednosti su {lat}");
-				//??? Problem 
-				
-				
-
-				
-			}
-            
-
          
 		}
+
+		public async Task OnPostAsync(double lat, double lon, string stopName) {
+
+			using (var httpClient = new HttpClient())
+			{
+				try
+				{
+					using (HttpResponseMessage response = await httpClient.GetAsync("https://localhost:7151/Stop/AllStops"))
+					{
+						string apiResponse = await response.Content.ReadAsStringAsync();
+						var _stops = (JsonConvert.DeserializeObject<List<StopGetBasicDTO>>(apiResponse)).ToList();
+
+						stops = _stops.ToList();
+
+						foreach (var s in stops)
+						{
+							if (s.Name == stopName)
+							{
+								ErrorMessage = "Već postoji stanica sa ovim imenom";
+								return;
+							}
+						}
+							
+							if (lat == 0 || lon == 0)
+							{
+								ErrorMessage = "Izaberite mesto za dodavanje stanica";
+							}
+							else
+							{
+								StopPostDTO newStop = new StopPostDTO()
+								{
+									Name = stopName,
+									Lat = lat,
+									Lon = lon
+								};
+
+								var _httpClient = new HttpClient();
+								var result = await _httpClient.PostAsync("https://localhost:7151/Stop", new StringContent(JsonSerializer.Serialize(newStop), Encoding.UTF8, "application/json"));
+
+								ErrorMessage = "Uspešno dodata stanica";
+
+							}
+
+							
+						
+
+					}
+				}
+				catch (Exception e)
+				{
+					ErrorMessage = "Lista stanica je trenutno nedostupna, pokušajte kasnije";
+				}
+			}
+
+			
+			
+
+		}
+
 	}
 }
